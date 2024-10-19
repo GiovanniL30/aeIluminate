f1968a0ca865ee9274a0dfd89942a2e4f2a6b79f
Jhea Prudencio <2232799@slu.edu.ph>
2024-10-19 10:05:21 +0800

    added delete.php

---
+++ backend/delete.php
@@ -0,0 +1,17 @@
+<?php
+include('../backend/database.php');
+
+if (isset($_POST['deleteUser'])) {
+    $userIdToDelete = $_POST['deleteUser'];
+
+    $deleteQuery = "DELETE FROM users WHERE userID = ?";
+    $stmt = $conn->prepare($deleteQuery);
+    $stmt->bind_param("i", $userIdToDelete);
+
+    if ($stmt->execute()) {
+        echo json_encode(['message' => 'User deleted succesfully']);
+    } else {
+        echo json_encode(['message' => 'Error. Failed to delete user']);
+    }
+    exit();
+}
